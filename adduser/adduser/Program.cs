14acc05d606aa1cc6233282f3ba470a1fd75ddb5
Katsec <47964489+Katsec@users.noreply.github.com>
2022-03-29 09:44:51 +0800

    Add files via upload

---
+++ adduser/adduser/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.DirectoryServices;
+
+class adduser
+{
+    static void Adduser(string username,string password ,string group) {
+        //string username = "";
+        //string password = "";
+        //string group = "";
+
+        try
+        {
+            DirectoryEntry AD = new DirectoryEntry("WinNT://" +
+            Environment.MachineName + ",computer");
+            DirectoryEntry NewUser = AD.Children.Add(username, "user");
+            NewUser.Invoke("SetPassword", new object[] { password });
+            NewUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+            NewUser.CommitChanges();
+            DirectoryEntry grp;
+
+            grp = AD.Children.Find(group, "group");
+            if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+            Console.WriteLine("Account Created Successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("author：Kat");
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Usage：adduser.exe <username> <password> <group>");
+        }
+        else {
+            string username = args[0];
+            string password = args[1];
+            string group = args[2];
+            Adduser(username, password, group);
+        
+        }
+    
+        
+    }
+}
